d163c36152d56e260fa2978db5653e19660312e4
kbeksultan <bkakpanbay@gmail.com>
2019-10-05 17:10:32 +0600

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+ 
+import {HomeComponent} from './home/home.component';
+import {MapComponent} from './map/map.component';
+ 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:  HomeComponent},
+  { path: 'map', component:  MapComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
